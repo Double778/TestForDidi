187e0d7777a36d51b1fa6b3891aee8dd61f8a3d8
Double778 <596638119@qq.com>
2016-06-12 16:42:19 +0800

    saDADASDASF

--- TestForDidi/app/src/main/java/com/zhao/testfordidi/login/LoginActivity.java
+++ TestForDidi/app/src/main/java/com/zhao/testfordidi/login/LoginActivity.java
@@ -1,9 +1,17 @@
 package com.zhao.testfordidi.login;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by 华哥哥 on 16/6/12.
  */
 public class LoginActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
 }
