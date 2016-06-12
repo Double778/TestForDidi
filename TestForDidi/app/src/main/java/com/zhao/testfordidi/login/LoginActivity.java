e1703494349952af46e7402c3ec3e9cf3286ec04
Double778 <596638119@qq.com>
2016-06-12 16:41:04 +0800

    denglu

---
+++ TestForDidi/app/src/main/java/com/zhao/testfordidi/login/LoginActivity.java
@@ -0,0 +1,9 @@
+package com.zhao.testfordidi.login;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 华哥哥 on 16/6/12.
+ */
+public class LoginActivity extends AppCompatActivity{
+}
