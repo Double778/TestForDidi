df11228296f5a31d748f36fa3b853dd55803c57e
韩旭 <982810829@qq.com>
2016-06-12 16:41:22 +0800

    66

---
+++ TestForDidi/app/src/main/java/com/zhao/testfordidi/fenxiang/Activity.java
@@ -0,0 +1,11 @@
+package com.zhao.testfordidi.fenxiang;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 16/6/12.
+ */
+public class Activity extends AppCompatActivity {
+
+    //组长竟逼事
+}
