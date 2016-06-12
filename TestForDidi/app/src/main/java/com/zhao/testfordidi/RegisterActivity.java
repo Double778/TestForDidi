a63d21ad9793a4ceb96f5c068a53eeef9626f6b3
巴为焱 <1160053654@qq.com>
2016-06-12 16:41:43 +0800

    注册界面

---
+++ TestForDidi/app/src/main/java/com/zhao/testfordidi/RegisterActivity.java
@@ -0,0 +1,9 @@
+package com.zhao.testfordidi;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ${巴为焱} on 16/6/12.
+ */
+public class RegisterActivity extends AppCompatActivity{
+}
